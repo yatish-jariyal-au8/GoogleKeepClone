22d5fcdcdb2746661fb2f9e792f521e5cfe1c8e6
Yatish Jariyal <yatishjariyal@gmail.com>
2020-11-16 13:31:49 +0530

    displaying notes and close button feature added

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ class App {
         this.notes = []
 
         this.$form = document.querySelector('#form')
+        this.$notes = document.querySelector('#notes')
         this.$noteTitle = document.querySelector('#note-title')
         this.$noteText = document.querySelector("#note-text");
         this.$formButtons = document.querySelector('#form-buttons')
+        this.$placeholder = document.querySelector('#placeholder')
+        this.$closeButton = document.querySelector('#form-close-button')
 
         this.addEventListeners()
     }
@@ -19,7 +22,6 @@ class App {
             event.preventDefault()
             const title = this.$noteTitle.value
             const text = this.$noteText.value
-
             const hasNote = title || text
 
             if (hasNote) {
@@ -27,13 +29,24 @@ class App {
                 this.addNote({title, text})
             }
         })
+
+        this.$closeButton.addEventListener('click', event => {
+            event.stopPropagation()
+            this.closeForm()
+        })
     }
 
     handleFormClick(event) {
         const isFormClicked = this.$form.contains(event.target)
 
+        const title = this.$noteTitle.value
+        const text = this.$noteText.value
+        const hasNote = title || text
+
         if (isFormClicked) {
             this.openForm()
+        } else if (hasNote) {
+            this.addNote({title, text})
         } else {
             this.closeForm()
         }
@@ -49,18 +62,40 @@ class App {
         this.$form.classList.remove('form-open')
         this.$noteTitle.style.display = 'none'
         this.$formButtons.style.display = 'none'
+        this.$noteTitle.value = ''
+        this.$noteText.value = ''
     }
 
-    addNote(note) {
+    addNote({title, text}) {
         const newNote = {
-            title: note.title,
-            text: note.text,
+            title,
+            text,
             color: 'white',
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         }
         this.notes = [...this.notes, newNote]
-        console.log(this.notes)
+        this.displayNotes()
+        this.closeForm()
     }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0;
+        this.$placeholder.style.display = hasNotes ? "none" : "flex"
+
+        this.$notes.innerHTML = this.notes.map(note => `
+        <div style="background: ${note.color}" class="note">
+            <div class="${note.title && 'note-title'}">${note.title}</div>
+            <div class="note-text">${note.text}</div>
+            <div class="toolbar-container">
+                <div class="toolbar">
+                    <img class="toolbar-color" src="https://icon.now.sh/palette">
+                    <img class="toolbar-delete" src="https://icon.now.sh/delete">
+                </div>
+            </div>
+        </div>
+        `).join("")
+    }
+    
 }
 
 new App()
